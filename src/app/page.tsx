338525810b81bfc27c74223ea1bedef812a6607f
Fahmi <fahminabill40@gmail.com>
2026-01-26 14:23:54 +0700

    ai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -829,10 +829,10 @@ export default function HomePage() {
                       />
                     ),
                     ul: ({ node, ...props }) => (
-                      <ul className="list-disc ml-4 space-y-1" {...props} />
+                      <ul className="list-disc pl-4 space-y-1" {...props} />
                     ),
                     ol: ({ node, ...props }) => (
-                      <ol className="list-decimal ml-4 space-y-1" {...props} />
+                      <ol className="list-decimal pl-4 space-y-1" {...props} />
                     ),
                     li: ({ node, ...props }) => (
                       <li className="pl-1" {...props} />
